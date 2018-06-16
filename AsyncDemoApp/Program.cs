8b936bad92169165dd4c8e1902114c2da2149c00
Ben Shepherd <b.c.shepherd@hotmail.com>
2018-06-16 22:22:40 +0100

    First commit.

--- AsyncDemoApp/Program.cs
+++ AsyncDemoApp/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace AsyncDemoApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Run();
+            Console.ReadLine();
+        }
+
+        async static void Run()
+        {
+            Task<int> getRandomNumberTask = GetRandomNumberTask();
+
+            IndependentMethod();
+
+            var randomNumber = await getRandomNumberTask;
+
+            Console.WriteLine($"Random number: {randomNumber}");
+        }
+
+        static async Task<int> GetRandomNumberTask()
+        {
+            await Task.Delay(3000);
+            return new Random().Next(0, 1500);
+        }
+
+        static void IndependentMethod()
+        {
+            Console.WriteLine("Please standby...");
         }
     }
 }
